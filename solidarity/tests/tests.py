a16842fd9c727c59c96003cec5d6d26c38c1c101
Amen Souissi <amensouissi@ecreall.com>
2018-09-27 11:02:16 +0200

    include BACKEND tests

--- solidarity/tests/tests.py
+++ solidarity/tests/tests.py
@@ -1,12 +1,20 @@
 import unittest
+import os
 
 from pyramid import testing
+from pyramid.paster import get_appsettings
 
 
 class FunctionalTests(unittest.TestCase):
+
+
     def setUp(self):
         from solidarity import main
-        app = main({})
+        config_file = os.getcwd() + '/../../pytest.ini'
+        settings = dict(get_appsettings(config_file, name='main'))
+        app = main({}, **settings)
+        request = testing.DummyRequest()
+        self.config = testing.setUp(request=request)
 
     def test_root(self):
         self.assertTrue(True)
